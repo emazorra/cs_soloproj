9517c07df0985b62365a63f885024b4f938fd0ff
Elizabeth Mazorra Vidal <emazorra1@gmail.com>
2024-03-28 13:58:41 -0400

    started creating webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './client/main.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exlude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+    title: 'development',
+    template: 'indexedDB.html'
+})],
+devServer: {
+    static: {
+        directory: path.resolve(__dirname, 'build'),
+        publicPath: '/build'
+    },
+    compress: true,
+    proxy: {
+        '/': 'http://localhost:3000'
+    },
+    port: 8080,
+}
+};
