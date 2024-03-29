0351b29a0a35f52e6d735652e03cd476811dec01
Elizabeth Mazorra Vidal <emazorra1@gmail.com>
2024-03-28 21:47:23 -0400

    got something to show on the front page

---
+++ client/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document
+      .querySelector('.page-login')
+      .querySelector('.container');
+
+})
+
+//request to sign in that the dom serves up
+//post request to database
