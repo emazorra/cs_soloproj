5e89f9052f35bbf31271e39251be1a528181ac93
Elizabeth Mazorra Vidal <emazorra1@gmail.com>
2024-09-29 19:43:28 -0400

    added signin page component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LoginForm from './login.jsx';
-import SignupForm from './signup.jsx';
-import Logo from '/docs/assets/images/Logo.png'
+
+
 
 const HomePage = () => {
     return (
@@ -10,16 +10,6 @@ const HomePage = () => {
             <div>
                 <LoginForm />
             </div>
-            <div>
-                <img
-                className="logo"
-                src={Logo}
-                ></img>
-            </div>
-            {/* <div> 
-                <h2> Or Sign Up</h2>
-                <SignupForm/>
-            </div>  */}
         </div>
     )
 }
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,19 +1,84 @@
 import React, { useState } from "react";
 import Profile from "./profile.jsx";
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Divider from '@mui/material/Divider';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import MuiCard from '@mui/material/Card';
+import { styled } from '@mui/material/styles';
+import '../loginpage.css';
+import ForgotPassword from '../ForgotPassword.js';
+import { GoogleIcon, SitemarkIcon } from '../CustomIcons.js';
+import AppTheme from '../shared-theme/AppTheme';
+import ColorModeSelect from '../shared-theme/ColorModeSelect';
+import logo from '../../docs/assets/images/Social Media Art 2logos.png'
+
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  [theme.breakpoints.up('sm')]: {
+    maxWidth: '450px',
+  },
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
+
+const SignInContainer = styled(Box)(({ theme }) => ({
+  padding: 20,
+  marginTop: '10vh',
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage: 
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
+  },
+}));
+
+
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [user, setUser] = useState(null);
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [open, setOpen] = useState(false);
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleClose = () => {
+    setOpen(false);
   };
 
   const handleSubmit = async (e) => {
@@ -48,39 +113,156 @@ const LoginForm = () => {
     }
   };
 
+  const validateInputs = () => {
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter a valid email address.');
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage('Password must be at least 6 characters long.');
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage('');
+    }
+
+    return isValid;
+  };
+
+
   return (
-    <div>
+    (<AppTheme>
+      <CssBaseline enableColorScheme />
       {!user ? (
-        <form onSubmit={handleSubmit}>
-          <Box className="login_form">
-            <h2>Log In</h2>
-            <label htmlFor="user_email">Email: </label>
-            <input
-              type="email"
-              id="user_email"
-              name="email"
-              placeholder="example@gmail.com"
-              onChange={handleUsernameChange}
+      <SignInContainer direction="column" justifyContent="space-between" className="login-form">
+        <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
+        <Card variant="outlined">
+          {/* <SitemarkIcon /> */}
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+          >
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              gap: 2,
+            }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <TextField
+                error={emailError}
+                helperText={emailErrorMessage}
+                id="email"
+                type="email"
+                name="email"
+                placeholder="your@email.com"
+                autoComplete="email"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={emailError ? 'error' : 'primary'}
+                sx={{ ariaLabel: 'email' }}
+              />
+            </FormControl>
+            <FormControl>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Link
+                  component="button"
+                  onClick={handleClickOpen}
+                  variant="body2"
+                  sx={{ alignSelf: 'baseline' }}
+                >
+                  Forgot your password?
+                </Link>
+              </Box>
+              <TextField
+                error={passwordError}
+                helperText={passwordErrorMessage}
+                name="password"
+                placeholder="••••••"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={passwordError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
             />
+            <ForgotPassword open={open} handleClose={handleClose} />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={validateInputs}
+            >
+              Sign in
+            </Button>
+            <Typography sx={{ textAlign: 'center' }}>
+              Don&apos;t have an account?{' '}
+              <span>
+                <Link
+                  href="/material-ui/getting-started/templates/sign-in/"
+                  variant="body2"
+                  sx={{ alignSelf: 'center' }}
+                >
+                  Sign up
+                </Link>
+              </span>
+            </Typography>
           </Box>
-          <Box className="login_form">
-            <label htmlFor="s_password">Password: </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={handlePasswordChange}
-            />
+          <Divider>or</Divider>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="outlined"
+              onClick={() => alert('Sign in with Google')}
+              startIcon={<GoogleIcon />}
+            >
+              Sign in with Google
+            </Button>
           </Box>
-          <button className="login_btn" type="submit">
-            Login
-          </button>
-        </form>
-      ) : (
+        </Card>
+      </SignInContainer>
+       ) : (
         <Profile {...user} />
       )}
+
+    <div className="logo-side">
+      <img src={logo} alt="Left Rising Logo" className="logo" />
     </div>
-  );
+    </AppTheme>
+
+  ));
 };
+
 
 export default LoginForm;
